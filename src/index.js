2a413abc3f78f010013c555489f62ad0a47b325c
nikitsch <79355999+nikitsch@users.noreply.github.com>
2021-08-23 23:41:57 +0300

    add: code index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  return array.reduce((a, b) => Math.min(a, b));
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  return array.reduce((a, b) =>  Math.max(a, b));
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  return array.reduce((a, b) => (a + b), 0) / array.length;
 }
